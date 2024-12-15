322de1a86fe2565983246ca3fd26c297d46eb594
RickyPonting2006 <rickyponting2006@gmail.com>
2024-12-15 23:16:04 +0530

    Update COUNTDOWN CLOCK (MINI PROJECT).py

--- COUNTDOWN CLOCK (MINI PROJECT).py
+++ COUNTDOWN CLOCK (MINI PROJECT).py
@@ -4,10 +4,9 @@ def countdown_timer(seconds):
     while seconds:
         mins, secs = divmod(seconds, 60)
         timer = f'{mins:02}:{secs:02}'
-        print(timer, end='\r')  # \r moves the cursor back to the beginning of the line
+        print(timer, end='\r')  
         time.sleep(1)
         seconds -= 1
-    print("Time's up!")  # Corrected the print statement
-
+    print("Time's up!")  
 seconds = int(input('Enter the time in seconds: '))
 countdown_timer(seconds)
